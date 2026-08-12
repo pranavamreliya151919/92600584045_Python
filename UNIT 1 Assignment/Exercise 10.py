5f3b905628c61596118bc9e1d7098fbeb8b4adf8
pranavamreliya151919 <pranav.amreliya151919@marwadiuniversity.ac.in>
2026-08-12 10:56:13 +0530

    Exercise 10.py

--- UNIT 1 Assignment/Exercise 10.py
+++ UNIT 1 Assignment/Exercise 10.py
@@ -1,3 +1,5 @@
+#10) Write a program to demonstrate recursion using factorial or Fibonacci series. 
+
 def factorial(n):
     if n == 0:
         return 1
@@ -5,5 +7,4 @@ def factorial(n):
         return n * factorial(n - 1)
 
 num = int(input("Enter a number: "))
-
 print("Factorial =", factorial(num))
